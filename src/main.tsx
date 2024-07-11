1110dcf22fa00ea38c42c94898cf5eeb2dda5183
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-10 23:01:50 -0500

    add provider for store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./_index.scss";
+import { StyledEngineProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </Provider>
+  </React.StrictMode>
+);
