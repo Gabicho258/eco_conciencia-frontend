dff3762abfa44a0a608dd1f8c3724c8fa03d42b4
Carlo <cdiazpor@unsa.edu.pe>
2024-07-12 13:44:40 -0500

    add user profile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login } from "./pages/Login/Login";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login/Login';
 
-import { HubHome } from "./pages/HubHome/HubHome";
-import HomePage from "./pages/HomePage/HomePage";
-import { Post } from "./pages/PostPage/Post";
+import { HubHome } from './pages/HubHome/HubHome';
+import HomePage from './pages/HomePage/HomePage';
+import { Post } from './pages/PostPage/Post';
+import { UserProfile } from './pages/UserProfile/UserProfile';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HubHome />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path='/' element={<HubHome />} />
+          <Route path='/home' element={<HomePage />} />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/post/" element={<Post />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/post/' element={<Post />} />
+          <Route path='/profile/' element={<UserProfile />} />
         </Routes>
       </BrowserRouter>
     </>
