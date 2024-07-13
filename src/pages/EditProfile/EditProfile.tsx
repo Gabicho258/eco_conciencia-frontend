770514737cbfa8988c7afd326ab75a389b9f5a79
Gabriel <gmamanican@unsa.edu.pe>
2024-07-12 19:46:31 -0500

    add connection to API for Post view

--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -1,99 +1,104 @@
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import AddIcon from '@mui/icons-material/Add';
-import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
-import './_EditProfile.scss';
-import { useNavigate } from 'react-router-dom';
-import { IUser } from '../../interfaces';
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import AddIcon from "@mui/icons-material/Add";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
+import "./_EditProfile.scss";
+import { useNavigate } from "react-router-dom";
+import { useGetUserByIdQuery } from "../../app/ecoCiencia.api";
 
 export const EditProfile = () => {
-  const user: IUser = {
-    _id: '123456789',
-    name: 'Juan Pérez',
-    email: 'juan.perez@example.com',
-    password: 'segura1234',
-    photo_url:
-      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-  };
+  const userDataStorage = localStorage.getItem("user_data");
+  const userCredentials =
+    userDataStorage?.includes("_id") && JSON.parse(userDataStorage);
+  const { data: user } = useGetUserByIdQuery(userCredentials?._id);
+  // const user: IUser = {
+  //   _id: '123456789',
+  //   name: 'Juan Pérez',
+  //   email: 'juan.perez@example.com',
+  //   password: 'segura1234',
+  //   photo_url:
+  //     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  // };
 
   const navigate = useNavigate();
 
   return (
     <>
-      <div className='editProfile'>
-        <div className='editProfile__buttons'>
+      <div className="editProfile">
+        <div className="editProfile__buttons">
           <Button
-            variant='contained'
-            className='editProfile__buttons-cancel'
+            variant="contained"
+            className="editProfile__buttons-cancel"
             onClick={() => navigate(-1)}
           >
             Cancelar
           </Button>
           <Button
-            variant='contained'
-            className='editProfile__buttons-save'
+            variant="contained"
+            className="editProfile__buttons-save"
             onClick={() => {}}
           >
             Guardar
           </Button>
         </div>
-        <div className='editProfile__main'>
-          <div className='editProfile__main-userImage'>
+        <div className="editProfile__main">
+          <div className="editProfile__main-userImage">
             <Avatar
-              className='editProfile__main-userImage-image'
+              className="editProfile__main-userImage-image"
               alt={user?.name}
               src={user?.photo_url}
             />
             <Button
-              variant='text'
-              className='editProfile__main-userImage-editImageBtn'
+              variant="text"
+              className="editProfile__main-userImage-editImageBtn"
             >
-              <AddIcon className='editProfile__main-userImage-editImageBtn-icon' />
+              <AddIcon className="editProfile__main-userImage-editImageBtn-icon" />
               <div
-                className='editProfile__main-userImage-editImageBtn-label'
+                className="editProfile__main-userImage-editImageBtn-label"
                 onClick={() => {}}
               >
                 Editar foto
               </div>
             </Button>
           </div>
-          <Divider className='editProfile__main-divider' />
-          <div className='editProfile__main-editForm'>
-            <div className='editProfile__main-editForm-grid'>
-              <div className='editProfile__main-editForm-grid-left'>
-                <div className='editProfile__main-editForm-grid-left-field'>
-                  <p className='editProfile__main-editForm-grid-left-field-label'>
+          <Divider className="editProfile__main-divider" />
+          <div className="editProfile__main-editForm">
+            <div className="editProfile__main-editForm-grid">
+              <div className="editProfile__main-editForm-grid-left">
+                <div className="editProfile__main-editForm-grid-left-field">
+                  <p className="editProfile__main-editForm-grid-left-field-label">
                     Correo:
                   </p>
                   <TextField
-                    className='editProfile__main-editForm-grid-left-field-input-disabled'
+                    className="editProfile__main-editForm-grid-left-field-input-disabled"
                     disabled
-                    id='outlined-disabled'
+                    id="outlined-disabled"
                     value={user?.email}
                     sx={{
-                      '& .MuiInputBase-root.Mui-disabled': {
-                        '& > fieldset': {
+                      "& .MuiInputBase-root.Mui-disabled": {
+                        "& > fieldset": {
                           opacity: 0,
                         },
                       },
                     }}
                   />
                 </div>
-                <div className='editProfile__main-editForm-grid-left-field'>
-                  <p className='editProfile__main-editForm-grid-left-field-label'>
+                <div className="editProfile__main-editForm-grid-left-field">
+                  <p className="editProfile__main-editForm-grid-left-field-label">
                     Nombre:
                   </p>
-                  <div className='editProfile__main-editForm-grid-left-field-inputContainer'>
+                  <div className="editProfile__main-editForm-grid-left-field-inputContainer">
                     <input
-                      type='text'
-                      className='editProfile__main-editForm-grid-left-field-inputContainer-input'
+                      defaultValue={user?.name}
+                      type="text"
+                      className="editProfile__main-editForm-grid-left-field-inputContainer-input"
                       onChange={() => {}}
                     />
                   </div>
                 </div>
               </div>
-              <div className='editProfile__main-editForm-grid-right'>
+              <div className="editProfile__main-editForm-grid-right">
                 {/* <div className="editProfile__main-editForm-grid-right-field">
                   <p className="editProfile__main-editForm-grid-right-label">
                     Nueva contraseña
