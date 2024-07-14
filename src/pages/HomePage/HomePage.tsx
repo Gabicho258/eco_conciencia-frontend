4f013718d19e8b039ca69ae3e7b7dbe426dfe099
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-13 22:00:20 -0500

    add no post handle

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,7 +6,7 @@ import { Box, Tab, Tabs } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSearch } from "../../hooks/useSearch";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
-
+import { categories } from "../../utils/constants";
 function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
@@ -15,7 +15,7 @@ function a11yProps(index: number) {
 }
 
 export default function HomePage() {
-  const categories = ["Todas", "Categoria1", "Categoria2", "Categoria3"];
+  // const categories = ["Todas", "Categoria1", "Categoria2", "Categoria3"];
   const { data: posts, refetch: refetchPosts } = useGetAllPostsQuery();
   const [value, setValue] = useState(0);
 
@@ -33,29 +33,49 @@ export default function HomePage() {
   return (
     <>
       <NavBar />
-      <div className="homePage">
-        <div className="homePage__categories">
-          <Box sx={{ width: "80%" }}>
-            {/* <Box sx={{ borderBottom: 1, borderColor: "divider" }}> */}
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-            >
-              {categories?.map((category, index) => (
-                <Tab label={category} {...a11yProps(index)} key={index} />
-              ))}
-            </Tabs>
-            {/* </Box> */}
-          </Box>
-          <SearchBar text={text} onChangeInput={onChangeInput} />
+      {posts?.length === 0 ? (
+        <h2 style={{ margin: "2rem 4rem" }}>Aún no hay publicaciones</h2>
+      ) : (
+        <div className="homePage">
+          <div className="homePage__categories">
+            <Box sx={{ width: "80%" }}>
+              {/* <Box sx={{ borderBottom: 1, borderColor: "divider" }}> */}
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons="auto"
+                aria-label="basic tabs example"
+                className="homePage__categories-tabs"
+                TabIndicatorProps={{
+                  style: {
+                    backgroundColor: "#D97D54",
+                  },
+                }}
+              >
+                {categories?.map((category, index) => (
+                  <Tab
+                    className="homePage__categories-tabs-tab"
+                    label={category}
+                    {...a11yProps(index)}
+                    key={index}
+                    // sx={{
+                    //   color: value === index ? "#FF0000" : "#bbff00", // Cambia a tu color deseado
+                    // }}
+                  />
+                ))}
+              </Tabs>
+              {/* </Box> */}
+            </Box>
+            <SearchBar text={text} onChangeInput={onChangeInput} />
+          </div>
+          <div className="homePage__cards">
+            {result?.map((post) => (
+              <PostCard post={post} key={post._id} />
+            ))}
+          </div>
         </div>
-        <div className="homePage__cards">
-          {result?.map((post) => (
-            <PostCard post={post} key={post._id} />
-          ))}
-        </div>
-      </div>
+      )}
     </>
   );
 }
